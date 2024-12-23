fad6a1b20e9570598107dfdf1cd4034e73842c55
Lauri <laurikamm@gmail.com>
2024-12-23 17:27:55 +0200

    implement question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import QuestionForm from "@/components/forms/QuestionForm";
+
 const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+  return (
+    <>
+      <div>
+        <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+        <div className="mt-9">
+          <QuestionForm />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AskQuestion;
