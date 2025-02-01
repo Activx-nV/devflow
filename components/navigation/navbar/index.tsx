cf0f3dedfc742b13c4e5abc3d655c9d2406b06a5
Lauri <laurikamm@gmail.com>
2025-02-01 15:08:36 +0200

    feat: implement log out and show user info on home

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import Theme from "./Theme";
 import MobileNavigation from "./MobileNavigation";
+import { auth } from "@/auth";
+import UserAvatar from "@/components/UserAvatar";
+import UserAvatar from "@/components/UserAvatar";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 sm:px-12 dark:shadow-none">
       <Link href="/" className="flex items-center gap-1">
@@ -22,6 +27,14 @@ const Navbar = () => {
 
       <div className="flex-between gap-5">
         <Theme />
+
+        {session?.user?.id && (
+          <UserAvatar
+            id={session?.user?.id}
+            name={session?.user?.name!}
+            imageUrl={session.user?.image!}
+          />
+        )}
 
         <MobileNavigation />
       </div>
