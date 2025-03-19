c0a9db4514eeb4b1f2bd651f8c9c980ba8e72781
Lauri <laurikamm@gmail.com>
2025-03-19 21:06:05 +0200

    implement user answers

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -1,11 +1,18 @@
-import React, { Suspense } from "react";
-import UserAvatar from "../UserAvatar";
 import Link from "next/link";
+import { Suspense } from "react";
+
 import ROUTES from "@/constants/routes";
-import { getTimeStamp } from "@/lib/utils";
-import { Preview } from "../editor/Preview";
-import Votes from "../votes/Votes";
 import { hasVoted } from "@/lib/actions/vote.action";
+import { cn, getTimeStamp } from "@/lib/utils";
+
+import { Preview } from "../editor/Preview";
+import UserAvatar from "../UserAvatar";
+import Votes from "../votes/Votes";
+
+interface Props extends Answer {
+  containerClasses?: string;
+  showReadMore?: boolean;
+}
 
 const AnswerCard = ({
   _id,
@@ -14,12 +21,19 @@ const AnswerCard = ({
   createdAt,
   upvotes,
   downvotes,
-}: Answer) => {
-  const hasVotedPromise = hasVoted({ targetId: _id, targetType: "answer" });
+  question,
+  containerClasses,
+  showReadMore = false,
+}: Props) => {
+  const hasVotedPromise = hasVoted({
+    targetId: _id,
+    targetType: "answer",
+  });
 
   return (
-    <article className="light-border border-b py-10">
-      <span id={JSON.stringify(_id)} className="hash-span" />
+    <article className={cn("light-border border-b py-10", containerClasses)}>
+      <span id={`answer-${_id}`} className="hash-span" />
+
       <div className="mb-5 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
         <div className="flex flex-1 items-start gap-1 sm:items-center">
           <UserAvatar
@@ -31,32 +45,42 @@ const AnswerCard = ({
 
           <Link
             href={ROUTES.PROFILE(author._id)}
-            className="flex flex-col sm:flex-row sm:items-center max-sm:ml-1"
+            className="flex flex-col max-sm:ml-1 sm:flex-row sm:items-center"
           >
             <p className="body-semibold text-dark300_light700">
               {author.name ?? "Anonymous"}
             </p>
 
             <p className="small-regular text-light400_light500 ml-0.5 mt-0.5 line-clamp-1">
-              <span className="max-sm:hidden"> • </span>answered{" "}
-              {getTimeStamp(createdAt)}
+              <span className="max-sm:hidden"> • </span>
+              answered {getTimeStamp(createdAt)}
             </p>
           </Link>
         </div>
+
         <div className="flex justify-end">
           <Suspense fallback={<div>Loading...</div>}>
             <Votes
               targetType="answer"
               targetId={_id}
+              hasVotedPromise={hasVotedPromise}
               upvotes={upvotes}
               downvotes={downvotes}
-              hasVotedPromise={hasVotedPromise}
             />
           </Suspense>
         </div>
       </div>
 
       <Preview content={content} />
+
+      {showReadMore && (
+        <Link
+          href={`/questions/${question}#answer-${_id}`}
+          className="body-semibold relative z-10 font-space-grotesk text-primary-500"
+        >
+          <p className="mt-1">Read more...</p>
+        </Link>
+      )}
     </article>
   );
 };
